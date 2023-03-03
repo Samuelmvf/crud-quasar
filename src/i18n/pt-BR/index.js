26b72faa7e95a9de39b58266f8a9b82a6f5ad9fd
Samuel Maia <samuelmvf@gmail.com>
2023-03-03 12:25:55 -0300

    Implementação do dicionário para tela de cadastro de cliente

--- src/i18n/pt-BR/index.js
+++ src/i18n/pt-BR/index.js
@@ -2,6 +2,63 @@ export default {
   pages: {
     cliente: {
       cadastro: {
+        titulo: {
+          cadastrar: 'CADASTRO DE CLIENTE',
+          editar: 'EDITAR CLIENTE'
+        },
+        form: {
+          input: {
+            ativo: {
+              label: 'Ativo',
+            },
+            documento: {
+              label: 'Documento*',
+              rules: 'Preencha o documento corretamente'
+            },
+            email: {
+              label: 'Email*',
+              rules: 'Preencha o campo email corretamente'
+            },
+            nome: {
+              label: 'Nome*',
+              rules: 'Preencha o nome corretamente'
+            },
+            telefone: {
+              label: 'Telefone*',
+              rules: 'Preencha o telefone corretamente'
+            }
+          }
+        },
+        button: {
+          editar: {
+            label: 'Editar'
+          },
+          cadastrar: {
+            label: 'Cadastrar'
+          },
+          cancelar: {
+            label: 'Cancelar'
+          }
+        },
+        acao: {
+          atualizar: {
+            notificacao: {
+              sucesso: 'Cliente atualizado com sucesso.',
+              erro: 'Falha ao atualizar cliente. Tente novamente mais tarde.'
+            }
+          },
+          cadastrar: {
+            notificacao: {
+              sucesso: 'Cliente cadastrado com sucesso.',
+              erro: 'Falha ao cadastrar cliente. Tente novamente mais tarde.'
+            }
+          },
+          buscarCliente: {
+            notificacao: {
+              erro: 'Falha ao buscar cliente. Tente novamente mais tarde.'
+            }
+          }
+        }
       },
       lista: {
         titulo: 'CLIENTE',
